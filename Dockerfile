fae50ebf4625b333b6c341a9edf19f346d139ee9
Jeremy Nickurak <jeremy.nickurak@cybera.ca>
2019-12-17 10:36:05 -0700

    Allow dpretty container to function on git ranges

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
 FROM alpine:latest
 
-RUN apk add --update nodejs py-pip yarn python3 gcc python3-dev musl-dev
+RUN apk add --update nodejs py-pip yarn python3 gcc python3-dev musl-dev git bash
 
 RUN yarn global add prettier
 RUN pip3 install black
+
+ADD .gitconfig /root/.gitconfig
+
+ADD git-rapply /usr/local/bin/
+ADD dpretty.sh /usr/local/bin/
+RUN chmod a+x /usr/local/bin/dpretty.sh
---
+++ dpretty.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+SCRIPT="$(readlink -fn "$0")"
+
+set -e
+
+while getopts :g:r: OPT; do
+    case $OPT in
+      g)
+          GITDIR="$OPTARG"
+        ;;
+      r)
+          GITRANGE=$OPTARG
+        ;;
+      *)
+          echo "Invalid option: $OPTARG" >/dev/stderr
+          exit 1
+    esac
+done
+shift "$(expr "$OPTIND" - 1 )"
+
+fix_range() {
+    local GITDIR="$1"; shift
+    local GITRANGE="$1"; shift
+    pushd "$GITDIR" &>/dev/null
+    git-rapply "$SCRIPT" "$GITRANGE"
+    popd
+}
+
+fix_file() {
+    local F=$1; shift
+    BASE="$(basename "$F")"
+    SUFFIX="${BASE/#*./}"
+    case "$SUFFIX" in
+        py)
+            echo "Running black on $F"
+            black "$F"
+            ;;
+        js | html | css | md | json | jsx )
+            echo "Running prettier on $F"
+            prettier --write "$F"
+            ;;
+        *)
+            echo "Don't know how to strip $SUFFIX files like $F"
+            ;;
+    esac
+}
+
+if [ -n "$GITDIR" ]; then
+    if [ -z "$GITRANGE" ]; then
+        GITRANGE="@{upstream}.."
+    fi
+    echo "Applying to Git dir $GITDIR on range $GITRANGE"
+    fix_range "$GITDIR" "$GITRANGE"
+else
+    if [ -n "$GITRANGE" ]; then
+        echo "-r can't be used without -g" > /dev/stderr
+        exit 1
+    fi
+    echo "Applying file-by-file to $*"
+    for F in "$@"; do
+        fix_file "$F"
+    done
+fi
