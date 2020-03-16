09fcaab7b0064c6b1dc2a313e519c7ce4659e421
Jeremy Nickurak <jeremy.nickurak@cybera.ca>
2020-03-15 23:49:00 -0600

    Use environment variables instead of .gitconfig
    Using .gitconfig is tricky if we're not entirely sure what user will be
    active in the container -- for example, if we're using docker's --id
    option to avoid trampling over file ownership

--- dpretty.sh
+++ dpretty.sh
@@ -4,6 +4,11 @@ SCRIPT="$(readlink -fn "$0")"
 
 set -e
 
+export GIT_AUTHOR_NAME="Dpretty Formatter"
+export GIT_AUTHOR_EMAIL="dpretty-formatter@nonexistant.invalid"
+export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
+export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
+
 while getopts :g:r: OPT; do
     case $OPT in
         g)
