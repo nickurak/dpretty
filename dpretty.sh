4cb4470c27789559f1293122a880e7a96bb604a0
Jeremy Nickurak <jeremy@nickurak.ca>
2024-05-03 20:57:14 -0600

    dpretty.sh: unset some variables we use
    This avoids any risk of them being set externally

--- dpretty.sh
+++ dpretty.sh
@@ -9,6 +9,9 @@ export GIT_AUTHOR_EMAIL="dpretty-formatter@nonexistant.invalid"
 export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
 export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
 
+unset GITDIR
+unset GITRANGE
+
 while getopts :g:r: OPT; do
     case $OPT in
         g)
